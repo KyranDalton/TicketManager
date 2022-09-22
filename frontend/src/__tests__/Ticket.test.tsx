9fc2a1f7e4e2a13ca3cbc81202a4ecdbf5ec6283
Kyriaco Kyran Theo Dalton <kyrand@amazon.com>
2022-09-22 13:17:06 +0100

    [frontend] Add Ticket component

---
+++ frontend/src/__tests__/Ticket.test.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { describe, it, expect } from '@jest/globals';
+import { render, screen } from '@testing-library/react';
+import { Ticket } from '../components/Ticket';
+
+const ID = '1234-1234-1234-1234';
+const TITLE = 'Test Title';
+const DESCRIPTION = 'Test Description';
+const USER = 'kyran';
+const SEVERITY = 5;
+const STATUS = 'ASSIGNED';
+const DATE = new Date('09/20/2022');
+
+describe('Ticket', () => {
+    it('renders the correct information for admins', () => {
+        render(
+            <Ticket
+                ticketProps={{
+                    ticketId: ID,
+                    title: TITLE,
+                    description: DESCRIPTION,
+                    requester: USER,
+                    severity: SEVERITY,
+                    status: STATUS,
+                    createDate: DATE,
+                    lastModifiedBy: USER,
+                    lastModifiedDate: DATE,
+                }}
+                isAdmin={true}
+            />,
+        );
+
+        const title = screen.getByText(TITLE);
+        const description = screen.getByText(DESCRIPTION);
+        const severity = screen.getByText(SEVERITY.toString());
+        const requester = screen.getByText(new RegExp(USER));
+        const status = screen.getByText(new RegExp(STATUS));
+        const createdOn = screen.getByText(new RegExp(DATE.toUTCString()));
+        const editButton = screen.getByText(/Edit Ticket/);
+
+        expect(title).toBeInTheDocument();
+        expect(description).toBeInTheDocument();
+        expect(severity).toBeInTheDocument();
+        expect(requester).toBeInTheDocument();
+        expect(status).toBeInTheDocument();
+        expect(createdOn).toBeInTheDocument();
+        expect(editButton).toBeInTheDocument();
+
+        // Admins should be able to edit
+        expect(editButton).not.toHaveAttribute('disabled');
+    });
+
+    it('renders the correct information for non-admins', () => {
+        render(
+            <Ticket
+                ticketProps={{
+                    ticketId: ID,
+                    title: TITLE,
+                    description: DESCRIPTION,
+                    requester: USER,
+                    severity: SEVERITY,
+                    status: STATUS,
+                    createDate: DATE,
+                    lastModifiedBy: USER,
+                    lastModifiedDate: DATE,
+                }}
+                isAdmin={false}
+            />,
+        );
+
+        const title = screen.getByText(TITLE);
+        const description = screen.getByText(DESCRIPTION);
+        const severity = screen.getByText(SEVERITY.toString());
+        const requester = screen.getByText(new RegExp(USER));
+        const status = screen.getByText(new RegExp(STATUS));
+        const createdOn = screen.getByText(new RegExp(DATE.toUTCString()));
+        const editButton = screen.getByText(/Edit Ticket/);
+
+        expect(title).toBeInTheDocument();
+        expect(description).toBeInTheDocument();
+        expect(severity).toBeInTheDocument();
+        expect(requester).toBeInTheDocument();
+        expect(status).toBeInTheDocument();
+        expect(createdOn).toBeInTheDocument();
+        expect(editButton).toBeInTheDocument();
+
+        // Only admins should be able to edit
+        expect(editButton).toHaveAttribute('disabled');
+    });
+});
---
+++ frontend/src/components/Ticket.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { Badge, Card, Grid, Heading, Text, Flex, Divider, Button } from '@aws-amplify/ui-react';
+import { Ticket as TicketType } from '../types';
+
+interface TicketProps {
+    ticketProps: TicketType;
+    isAdmin: boolean;
+}
+
+export function Ticket({ ticketProps, isAdmin }: TicketProps) {
+    const { title, description, severity, requester, status, createDate } = ticketProps;
+
+    const [showEditModal, setShowEditModal] = useState(false);
+
+    return (
+        <>
+            <Card variation="outlined">
+                <Grid>
+                    <Flex columnStart={1} columnEnd={1} rowStart={1} rowEnd={2} direction="row">
+                        <Badge alignSelf="center" width="min-content">
+                            {severity}
+                        </Badge>
+                        <Heading alignSelf="center" level={5}>
+                            {title}
+                        </Heading>
+                        <Button
+                            alignSelf="center"
+                            marginLeft="auto"
+                            variation="primary"
+                            disabled={!isAdmin}
+                            onClick={() => setShowEditModal(true)}
+                        >
+                            Edit Ticket
+                        </Button>
+                    </Flex>
+                    <Flex columnStart={1} columnEnd={1} rowStart={2} rowEnd={3} paddingTop="1rem">
+                        <Text>{description}</Text>
+                    </Flex>
+                    <Flex columnStart={1} columnEnd={1} rowStart={3} rowEnd={4} paddingTop="1rem">
+                        <Text fontStyle="italic">Request by: {requester}</Text>
+                        <Divider orientation="vertical" />
+                        <Text fontStyle="italic">Status: {status}</Text>
+                        <Divider orientation="vertical" />
+                        <Text fontStyle="italic">Created on: {createDate.toUTCString()}</Text>
+                    </Flex>
+                </Grid>
+            </Card>
+        </>
+    );
+}
